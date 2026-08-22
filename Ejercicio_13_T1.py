641cb796a07ee7b8e25c6bf120a83ecd00295ffc
ederhoppe <eder.creaper@gmail.com>
2026-08-22 10:48:08 -0600

    V1: Script nuevo
    Script original

---
+++ Ejercicio_13_T1.py
@@ -0,0 +1,6 @@
+"""13. Escribe un programa que te permita ingresar el nombre de una película 
+y como salida muestre el último caracter de la cadena"""
+
+print('Bienvenido a la ultima letra')
+pelicula = input('Ingresa el nombre de una pelicula ')
+print(pelicula[-1])
